62aaad6ed73bfdad32bcbd65616f4e462304fa36
Yash-Roy <yashroy2240@gmail.com>
2026-07-01 22:11:13 +0530

    implement back button in plant game

--- src/pages/Plants/level2/SeedLesson.tsx
+++ src/pages/Plants/level2/SeedLesson.tsx
@@ -1,16 +1,41 @@
 import { useState } from "react";
 import { seedScenes } from "./seedData.ts";
+import { useEffect } from "react";
+
+import { useNavigate } from "react-router-dom";
 
 import seedImg from "../../../assets/Plants/level2/seed.png";
 import waterImg from "../../../assets/Plants/level2/seed-water.png";
 import sproutImg from "../../../assets/Plants/level2/seed-root.png";
 import rootsImg from "../../../assets/Plants/level2/small-plant.png";
 import plantImg from "../../../assets/Plants/level2/flower-plant.png";
+import owlLogo from "../../../assets/owl.png";
 
 export default function SeedLesson() {
+  const navigate = useNavigate();
+
   const [sceneIndex, setSceneIndex] = useState(0);
 
   const scene = seedScenes[sceneIndex];
+
+  const [currentTeacherMessage, setCurrentTeacherMessage] = useState(
+    scene.message,
+  );
+
+  useEffect(() => {
+    setCurrentTeacherMessage(scene.message);
+    speakMessage(scene.message);
+  }, [sceneIndex]);
+
+  const speakMessage = (message: string) => {
+    const speech = new SpeechSynthesisUtterance(message);
+
+    speech.rate = 0.9;
+    speech.pitch = 1.2;
+
+    speechSynthesis.cancel();
+    speechSynthesis.speak(speech);
+  };
 
   let currentImage = seedImg;
 
@@ -34,9 +59,42 @@ export default function SeedLesson() {
 
   return (
     <div className="min-h-screen bg-gradient-to-r from-green-400 to-emerald-500 px-4 py-6 pb-36 md:pb-24">
+      <div className="flex items-center">
+        <button
+          disabled={sceneIndex === 0}
+          onClick={() => {
+            if (sceneIndex > 0) {
+              setSceneIndex(sceneIndex - 1);
+            }
+          }}
+          className="
+    flex
+    items-center
+    gap-2
+    px-4
+    py-2
+    rounded-full
+    shadow-lg
+    transition
+    font-bold
+
+    disabled:opacity-50
+    disabled:cursor-not-allowed
+  "
+        >
+          ← Back
+        </button>
+      </div>
+
       {/* Title */}
-      <div className="text-center pt-8">
-        <h1 className="text-4xl md:text-5xl font-black text-green-800">
+      <div className="text-center mt-8">
+        <h1
+          className="text-3xl
+  sm:text-4xl
+  lg:text-5xl
+
+  font-black text-green-800"
+        >
           Seed Growth
         </h1>
 
@@ -50,66 +108,119 @@ export default function SeedLesson() {
         className="
   max-w-6xl
   mx-auto
-  mt-0
+
+  mt-8
+
   flex
   flex-col
-  md:flex-row
+  lg:flex-row
+
   items-center
   justify-center
-  gap-10
+
+  gap-8
+  lg:gap-20
+
+  px-4
 "
       >
         <img
-  src={currentImage}
-  alt={scene.title}
-  className="
-    w-30
-    sm:w-60
-    md:w-[100px]
-    lg:w-[100px]
+          src={currentImage}
+          alt={scene.title}
+          className="
+    w-32
+    sm:w-40
+    md:w-56
+    lg:w-45
+
+    h-auto
+
     object-contain
+
     transition-all
     duration-700
-"
-/>
-      </div>
-
-      {/* Teacher Card */}
-      <div className="max-w-md mx-auto mt-10">
+  "
+        />
         <div
           className="
-bg-white
-rounded-3xl
-shadow-2xl
-p-6
-md:p-8
-transition-all
-duration-300
-hover:scale-105
+  w-full
+  md:w-auto
+
+  flex
+  justify-center
 "
         >
-          <h2
+          <div
             className="
-text-2xl
-md:text-4xl
-font-black
-text-green-700
-"
-          >
-            {scene.title}
-          </h2>
+    bg-white
 
-          <p
-            className="
-mt-5
-text-lg
-md:text-xl
-leading-8
-text-gray-700
-"
+    rounded-3xl
+
+    shadow-2xl
+
+    p-5
+    md:p-7
+
+    w-full
+    w-full
+max-w-sm
+sm:max-w-md
+lg:max-w-lg
+
+    relative
+
+    transition-all
+    duration-500
+
+    hover:scale-105
+  "
           >
-            {scene.message}
-          </p>
+            <div className="flex items-center gap-3">
+              <img
+                src={owlLogo}
+                alt="Quester"
+                className="
+    w-12
+    h-12
+
+    md:w-16
+    md:h-16
+
+    object-contain
+
+   
+  "
+              />
+
+              <h3
+                className="
+    font-black
+    text-green-700
+
+    text-xl
+    md:text-3xl
+  "
+              >
+                Quester
+              </h3>
+            </div>
+
+            <p
+              className="
+    mt-4
+
+    text-base
+    md:text-xl
+
+    leading-7
+    md:leading-9
+
+    text-gray-700
+  "
+            >
+              {currentTeacherMessage}
+            </p>
+          </div>
         </div>
       </div>
 
@@ -118,16 +229,25 @@ text-gray-700
         <button
           onClick={() => {
             if (sceneIndex < seedScenes.length - 1) {
-              setSceneIndex(sceneIndex + 1);
+              setSceneIndex((prev) => prev + 1);
+            } else {
+              speechSynthesis.cancel();
+
+              // later
+              // navigate("/plants/level2/activity");
             }
           }}
           className="
         bg-yellow-400
         hover:bg-yellow-500
-        px-12
-        py-4
+        w-full
+max-w-xs
+
+py-4
+
+text-lg
+md:text-xl
         rounded-3xl
-        text-xl
         font-black
         shadow-xl
         "
